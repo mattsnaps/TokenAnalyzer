546fe85b0c47c09570bd8321db89e83a66f9c3ba
mattsnaps <priebemb@gmail.com>
2021-01-31 15:38:13 -0600

    finished lab 2, pushing changes up to github

---
+++ src/java112/labs1/MysteryClassOne.java
@@ -0,0 +1,22 @@
+package java112.labs1;
+/**
+ * Mystery Class Lab - first TDD of semester
+ * @author mbpriebe
+ */
+
+public class MysteryClassOne {
+    /**run Myster Class Program.
+    *Output results from mysterMethodOne
+    *@param arg command line arguments
+    */
+   public static void main(String[] args) {
+       MysteryClassOne lab2 = new MysteryClassOne();
+       System.out.println("The value is: " + lab2.mysteryMethodOne());
+   }
+    /**
+     * adding mystery method to pass the tests.
+    */
+   public int mysteryMethodOne() {
+        return 1;
+    }
+}
