3afd7b6ab23a8aef94629f0787c18aac8f4912fa
mattsnaps <priebemb@gmail.com>
2021-02-13 00:32:34 -0600

    Project 1 - Got file into an array and set.

--- src/java112/analyzer/FileAnalysis.java
+++ src/java112/analyzer/FileAnalysis.java
@@ -20,8 +20,55 @@ public class FileAnalysis {
         if (arguments.length != validArgs) {
             System.out.println("Please enter one argument on the command line");
         } else {
-            summaryAnalyzer = new FileSummaryAnalyzer();
-            distinctAnalyzer = new DistinctTokenAnalyzer();
+            instanceVariable();
+            openFile(arguments[0]);
         }
+    }
+
+    public void instanceVariable() {
+        summaryAnalyzer = new FileSummaryAnalyzer();
+        distinctAnalyzer = new DistinctTokenAnalyzer();
+    }
+    public void openFile(String fileName) {
+        try (BufferedReader input = new BufferedReader(new FileReader(fileName))) {
+            while (input.ready()) {
+                String line = input.readLine();
+                loopFile(line);
+                setLoop(line);
+            }
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println("File not Found...");
+            fileNotFound.printStackTrace();
+        } catch (IOException iOException) {
+            System.out.println("Failed to read file...");
+            iOException.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("Some Other Problem...");
+            exception.printStackTrace();
+        }
+    }
+    public void loopFile(String line) {
+        List<String> myList = new ArrayList<String>();
+        String[] splitLine = line.split("\\w");
+
+        for (String token : splitLine) {
+            myList.add(token);
+        }
+
+    }
+    public void setLoop(String line) {
+        Set<String> setList = new TreeSet<String>();
+        String[] splitLine = line.split("\\w");
+
+        for (String token : splitLine) {
+            setList.add(token);
+        }
+
+    }
+    public void passToProcessToken() {
+
+    }
+    public void writeOutputFiles() {
+
     }
 }
