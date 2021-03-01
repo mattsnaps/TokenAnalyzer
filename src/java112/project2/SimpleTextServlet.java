e41adf49be72b4c0dfbf08b87bdd91bb13e085fb
mattsnaps <priebemb@gmail.com>
2021-02-28 19:37:18 -0600

    Done with servlets video. About to work on lab 5

---
+++ src/java112/project2/SimpleTextServlet.java
@@ -0,0 +1,43 @@
+package java112.project2;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+/**
+ *  This is part of a lab and is the first servlet for the course.
+ *
+ *@author    eknapp
+ */
+@WebServlet(
+    name = "simpleServlet",
+    urlPatterns = { "/simpleText"}
+)
+public class SimpleTextServlet extends HttpServlet {
+
+    /**
+     *  Handles HTTP GET requests.
+     *
+     *@param  request                   the HttpServletRequest object
+     *@param  response                   the HttpServletResponse object
+     *@exception  ServletException  if there is a Servlet failure
+     *@exception  IOException       if there is an IO failure
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        // set the response type before sending data
+        PrintWriter  out  = response.getWriter();
+        out.print("<HTML>");
+        out.print("<HEAD><TITLE>TrivialServlet Output</TITLE></HEAD>");
+        out.print("<BODY>");
+        out.print("<h1>Is this Better!!!!!!?</h1>");
+        System.out.println("Is this logging <= This is from sout?");
+        log("Is this logging? <= is from the log");
+        out.print("</BODY>");
+        out.print("</HTML>");
+        out.close();
+    }
+
+}
