bafc8ea6c07cb75ee7a1d4bb74baa78485a40143
mattsnaps <priebemb@gmail.com>
2021-05-09 16:21:25 -0500

    Added the search class for project 4

---
+++ src/java112/employee/Search.java
@@ -0,0 +1,94 @@
+package java112.employee;
+import java.util.*;
+/**
+ * @author mbpriebe
+ *
+ */
+
+public class Search {
+    private String searchType;
+    private String searchTerm;
+    private ArrayList<String> results;
+    private Boolean response;
+
+    /**
+     * Constructor Class
+     *
+     */
+    public Search() {
+
+    }
+
+    /**
+	* Returns value of searchType
+	* @return
+	*/
+	public String getSearchType() {
+		return searchType;
+	}
+
+	/**
+	* Sets new value of searchType
+	* @param
+	*/
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	/**
+	* Returns value of searchTerm
+	* @return
+	*/
+	public String getSearchTerm() {
+		return searchTerm;
+	}
+
+	/**
+	* Sets new value of searchTerm
+	* @param
+	*/
+	public void setSearchTerm(String searchTerm) {
+		this.searchTerm = searchTerm;
+	}
+
+	/**
+	* Returns value of results
+	* @return
+	*/
+	public ArrayList<String> getResults() {
+		return results;
+	}
+
+	/**
+	* Sets new value of results
+	* @param
+	*/
+	public void setResults(ArrayList<String> results) {
+		this.results = results;
+	}
+
+	/**
+	* Returns value of response
+	* @return
+	*/
+	public Boolean getResponse() {
+		return response;
+	}
+
+	/**
+	* Sets new value of response
+	* @param
+	*/
+	public void setResponse(Boolean response) {
+		this.response = response;
+	}
+
+    /**
+     * [addFoundEmployee description]
+     * @param employee [description]
+     */
+    public void addFoundEmployee(Employee employee) {
+
+    }
+
+}
